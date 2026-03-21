7d1d510c020614ab0e9c74047f4f00816168e921
Jeeyson <jasonezezel@gmail.com>
2026-03-20 18:54:47 -0600

    creacion de componentes

--- AppPlanilla/src/app/app.routes.ts
+++ AppPlanilla/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Empleados } from './empleados/empleados';
+import { Panel } from './panel/panel';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'empleados',component:Empleados,
+        
+    },
+    {
+        path: 'panel',component:Panel,
+    }
+];
