5f8aa6431ae6778bd16e40e3a43d5845692d116c
FranciscoDavila09 <abernydavila16@gmail.com>
2026-03-21 18:12:34 -0600

    Subiendo lo que se trabajo viernes 19, se genero las distintas carpetas donde estan los componentes

--- AppPlanilla/src/app/app.routes.ts
+++ AppPlanilla/src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
 import { Empleados } from './empleados/empleados';
 import { Panel } from './panel/panel';
+import { Asistencia } from './asistencia/asistencia';
+import { Horarios } from './horarios/horarios';
+import { Planilla } from './planilla/planilla';
+import { Pagos } from './pagos/pagos';
+import { Prestamos } from './prestamos/prestamos';
+import { Deducciones } from './deducciones/deducciones';
 
 export const routes: Routes = [
     {
@@ -9,5 +15,23 @@ export const routes: Routes = [
     },
     {
         path: 'panel',component:Panel,
+    },
+    {
+        path: 'asistencia', component: Asistencia,
+    },
+    {
+        path: 'horarios', component: Horarios,
+    },
+    {
+        path: 'planilla', component: Planilla,
+    },
+    {
+        path: 'pagos', component: Pagos,
+    },
+    {
+        path: 'prestamos', component: Prestamos,
+    },
+    {
+        path: 'deducciones', component: Deducciones,
     }
 ];
