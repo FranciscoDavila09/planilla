f39ab6f8f432cb792913d2570b9d381408e52fde
Jeeyson <jasonezezel@gmail.com>
2026-03-21 20:28:02 -0600

    creacion de componente empleado

--- AppPlanilla/src/app/app.routes.ts
+++ AppPlanilla/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Empleados } from './empleados/empleados';
 import { Panel } from './panel/panel';
+import { Puestos } from './puestos/puestos';
 
 export const routes: Routes = [
     {
@@ -9,5 +10,9 @@ export const routes: Routes = [
     },
     {
         path: 'panel',component:Panel,
-    }
+    },
+    {
+        path: 'puestos',component:Puestos,
+    },
+    
 ];
